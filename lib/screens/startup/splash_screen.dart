14a4a58e6ff728cf2f5b9f70780c24c24adf1c35
adityor6 <adityor6@gmail.com>
2022-11-24 20:58:35 +0700

    login screen

--- lib/screens/startup/splash_screen.dart
+++ lib/screens/startup/splash_screen.dart
@@ -5,6 +5,13 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+
+body: Center(child: Text("ede")),
+
+
+
+
+    );
   }
 }
