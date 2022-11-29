04d6e80995784c3b7109ae34a44a54c5c1551ae5
Kharisma FA <kharismavv@students.amikom.ac.id>
2022-11-29 16:56:03 +0700

    add dashboard/home page

--- lib/routes/route_generator.dart
+++ lib/routes/route_generator.dart
@@ -3,6 +3,7 @@ import 'package:hospital_management_system/routes/route_builder.dart';
 import 'package:hospital_management_system/routes/route_names.dart';
 import 'package:hospital_management_system/screens/add_patient_data/add_patient_data_screen.dart';
 import 'package:hospital_management_system/screens/auth/login_screen.dart';
+import 'package:hospital_management_system/screens/home/home_screen.dart';
 import 'package:hospital_management_system/screens/patient_data/patient_data_screen.dart';
 
 import '../screens/profile/profile_screen.dart';
@@ -26,6 +27,8 @@ class RouteGenerator {
         return RouteBuilder(child: const PatientDataScreen());
       case RouteNames.addPatientData:
         return RouteBuilder(child: const AddPatientDataScreen());
+      case RouteNames.home:
+        return RouteBuilder(child: HomeScreen());
 
       default:
         return _errorRoute();
