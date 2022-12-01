466f615d5185593f3d1b7ac822d0dd0db697a865
adityor6 <adityor6@gmail.com>
2022-12-01 21:25:17 +0700

    splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:hospital_management_system/routes/route_generator.dart';
 import 'package:hospital_management_system/routes/route_names.dart';
+import 'package:hospital_management_system/screens/auth/login_screen.dart';
+import 'package:hospital_management_system/screens/startup/splash_screen.dart';
+import 'package:hospital_management_system/utils/constant.dart';
+import 'package:splash_screen_view/SplashScreenView.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,30 +15,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      // theme: ThemeData(
-      //   bottomNavigationBarTheme: BottomNavigationBarThemeData(
-      //     selectedItemColor: Colors.red,
-      //     unselectedItemColor: Colors.grey,
-      //     backgroundColor: Colors.red,
-      //     type: BottomNavigationBarType.fixed,
-      //   ),
-      //   tabBarTheme: TabBarTheme(
-      //     indicator: BoxDecoration(
-      //       color: Color.fromARGB(255, 136, 157, 174),
-      //       borderRadius: BorderRadius.circular(50),
-
-      //     ),
-      //     unselectedLabelColor: Colors.black,
-      //     labelStyle: TextStyle(
-      //       fontSize: 16,
-      //       fontWeight: FontWeight.bold,
-      //     ),
-      //   ),
-      // ),
+    //  Widget splashscreen = SplashScreenView(
+    //   speed: 50,
+    //   pageRouteTransition: PageRouteTransition.CupertinoPageRoute,
+    //   backgroundColor: Constant.lightColor,
+    //   colors: [Colors.blue],
+    //   navigateRoute: LoginScreen(),
+    //   duration: 500,
+    //   imageSize: 600,
+    //   imageSrc:
+    //       "https://images.pexels.com/photos/14617972/pexels-photo-14617972.jpeg",
+    // );
+    
+    
+    
+    return MaterialApp(
+    
       debugShowCheckedModeBanner: false,
-      initialRoute: RouteNames.login,
+      // initialRoute: RouteNames.splashScreen, 
       onGenerateRoute: RouteGenerator.generateRoute,
+      home: SplashScreen(),
     );
   }
 }
--- lib/screens/startup/splash_screen.dart
+++ lib/screens/startup/splash_screen.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:hospital_management_system/utils/constant.dart';
+import 'package:splash_screen_view/SplashScreenView.dart';
+
+import '../auth/login_screen.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-
-body: Center(child: Text("ede")),
-
-
-
-
+    return SplashScreenView(
+      speed: 50,
+      pageRouteTransition: PageRouteTransition.CupertinoPageRoute,
+      backgroundColor: Constant.lightColor,
+      colors: [Colors.blue],
+      navigateRoute: LoginScreen(),
+      duration: 500,
+      imageSize: 600,
+      imageSrc:
+          "https://images.pexels.com/photos/14617972/pexels-photo-14617972.jpeg",
     );
   }
 }
