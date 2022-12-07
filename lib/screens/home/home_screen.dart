b1892cb32fb590b1dd06176029d16c3df17cb5b4
tuan_muda_indar <ismunandarmuis311@gmail.com>
2022-12-07 15:01:29 +0800

    update

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:hospital_management_system/models/doctor_model.dart';
 import 'package:hospital_management_system/routes/route_names.dart';
-import 'package:hospital_management_system/screens/home/widgets/list_doctor_card.dart';
-import 'package:hospital_management_system/screens/home/widgets/pasient_count_card.dart';
 import '../../utils/constant.dart';
+import '../../models/doctor_model.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  HomeScreen({Key? key}) : super(key: key);
+
+  //list object doctor
+  final List<DoctorModel> doctors = [
+    DoctorModel(
+        id: 1,
+        name: "name",
+        specialityId: 1,
+        licenseNumber: "licenseNumber",
+        specialityName: "specialityName",
+        doctorSchedules: []),
+  ];
+
+  // list data doctor
 
   @override
   Widget build(BuildContext context) {
@@ -17,22 +28,8 @@ class HomeScreen extends StatelessWidget {
         child: AppBar(
           backgroundColor: Constant.lightColor,
           toolbarHeight: 90,
-          leading: Transform.translate(
-            offset: const Offset(15, 0),
-            child: Container(
-              width: 48,
-              height: 48,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                  image: AssetImage('assets/images/profile.jpg'),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-          ),
           title: Padding(
-            padding: const EdgeInsets.only(left: 20),
+            padding: const EdgeInsets.only(left: 25),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -60,34 +57,181 @@ class HomeScreen extends StatelessWidget {
           ),
           actions: [
             IconButton(
-              onPressed: () {
-                Navigator.pushNamed(context, RouteNames.notification);
-              },
+              onPressed: () =>
+                  Navigator.pushNamed(context, RouteNames.notification),
               icon: const Icon(Icons.notifications),
             ),
           ],
-
+          leading: Transform.translate(
+            offset: const Offset(15, 0),
+            child: const CircleAvatar(
+              backgroundImage: AssetImage('assets/images/profile.jpg'),
+            ),
+          ),
           leadingWidth: 50,
-
-          //image profile
         ),
       ),
-      body: Container(
-        padding:
-            const EdgeInsets.symmetric(horizontal: Constant.horizontalPadding),
-        child: ListView(
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
           children: [
-            //padding top
-            const SizedBox(
-              height: Constant.verticalPadding,
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(7),
+                boxShadow: Constant.cardShadow,
+              ),
+              margin: const EdgeInsets.only(top: 5),
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 22),
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Pasien Hari Ini',
+                    style: Constant.primaryTextStyle.copyWith(
+                      fontWeight: Constant.boldFontWeight,
+                      fontSize: Constant.subtitleFontSize,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        height: 88,
+                        width: 140,
+                        decoration: BoxDecoration(
+                          color: Constant.lightestColor,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '8',
+                              style: Constant.primaryTextStyle.copyWith(
+                                fontWeight: Constant.boldFontWeight,
+                                fontSize: 28,
+                              ),
+                            ),
+                            Text(
+                              'Antrian',
+                              style: Constant.primaryTextStyle.copyWith(
+                                fontWeight: Constant.boldFontWeight,
+                                fontSize: Constant.bodyFontSize,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 88,
+                        width: 140,
+                        decoration: BoxDecoration(
+                          color: Constant.lightestColor,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '12',
+                              style: Constant.primaryTextStyle.copyWith(
+                                fontWeight: Constant.boldFontWeight,
+                                fontSize: 28,
+                              ),
+                            ),
+                            Text(
+                              'Telah Berkunjung',
+                              style: Constant.primaryTextStyle.copyWith(
+                                fontWeight: Constant.boldFontWeight,
+                                fontSize: Constant.bodyFontSize,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-            const PasienCountCard(),
             const SizedBox(height: 20),
-
-            ListDoctorCard(),
-
-            // padding bottom
-            const SizedBox(height: Constant.verticalPadding),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(7),
+                boxShadow: Constant.cardShadow,
+              ),
+              margin: const EdgeInsets.only(top: 1),
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 22),
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Dokter Tersedia',
+                    style: Constant.primaryTextStyle.copyWith(
+                      fontWeight: Constant.boldFontWeight,
+                      fontSize: Constant.subtitleFontSize,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  ListView.separated(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: doctors.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: const EdgeInsets.only(bottom: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                const CircleAvatar(
+                                  backgroundImage: AssetImage(
+                                      'assets/images/login_background.png'),
+                                  radius: 20,
+                                ),
+                                const SizedBox(width: 10),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      doctors[index].name,
+                                      style: Constant.primaryTextStyle.copyWith(
+                                        fontWeight: Constant.mediumFontWeight,
+                                        fontSize: Constant.bodyFontSize,
+                                      ),
+                                    ),
+                                    Text(
+                                      (doctors[index].doctorSchedules.isEmpty)
+                                          ? ''
+                                          : 'Tersedia pukul ${doctors[index].doctorSchedules[index].startTime} - ${doctors[index].doctorSchedules[index].endTime}',
+                                      style: Constant.primaryTextStyle.copyWith(
+                                        fontWeight: Constant.boldFontWeight,
+                                        fontSize: Constant.bodyFontSize,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return const Divider(
+                        color: Constant.darkColor,
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
